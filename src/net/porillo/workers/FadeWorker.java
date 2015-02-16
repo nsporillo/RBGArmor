b3807b3e6928f7ea7ca5c4512ec973bfd4708306
Nick Porillo <nsporillo@gmail.com>
2015-02-15 22:46:17 -0500

    New coloring worker
    This worker differs from the sync worker in that the next color is
    retrieved after each armor piece is colored, as opposed to the entire
    set being 1 color.
    
    This is actually a much more defined and interesting coloring scheme!

---
+++ src/net/porillo/workers/FadeWorker.java
@@ -0,0 +1,43 @@
+package net.porillo.workers;
+
+import java.util.UUID;
+
+import net.porillo.RainbowGear;
+
+import org.bukkit.Color;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+public class FadeWorker extends Worker {
+	
+	private int c = -1;
+	private final int t;
+	
+	public FadeWorker(UUID uuid) {
+		super(uuid);
+		this.t = (RainbowGear.rb.length - 1);
+	}
+
+	@Override
+	public void run() {
+		for (ItemStack is : inv.getArmorContents()) {
+			Color c = getNext();
+			if (is != null && is.getItemMeta() instanceof LeatherArmorMeta) {
+				if (!RainbowGear.isWorthy(is.getItemMeta())) {
+					continue;
+				}
+				LeatherArmorMeta lam = (LeatherArmorMeta) is.getItemMeta();
+				lam.setColor(c);
+				is.setItemMeta(lam);
+			}
+		}
+	}
+
+	
+	private Color getNext() {
+		if (c >= t)
+			c = -1;
+		return RainbowGear.rb[++c];
+	}
+
+}
