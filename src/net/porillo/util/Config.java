5f58b0c6efe9c557e7f3ab2a9ec2a0432e4b5a37
Nick Porillo <nsporillo@gmail.com>
2015-03-01 01:06:03 -0500

    Add integration settings

--- src/net/porillo/util/Config.java
+++ src/net/porillo/util/Config.java
@@ -4,23 +4,29 @@ import net.porillo.RBGArmor;
 
 public class Config {
 
+    private String storeUrl;
     private boolean debug;
+    private boolean requirePerms;
     private int refreshRate;
     private int colors;
 
     /**
      * Config constructor
-     * @param rgb main class 
+     * 
+     * @param rgb main class
      */
     public Config(RBGArmor rgb) {
         rgb.saveDefaultConfig();
-        colors = rgb.getConfig().getInt("Core.Colors", 64);
-        refreshRate = rgb.getConfig().getInt("Core.RefreshRate", 5);
-        debug = rgb.getConfig().getBoolean("Core.Debug", false);
+        colors = rgb.getConfig().getInt("core.colors", 64);
+        refreshRate = rgb.getConfig().getInt("core.refresh-rate", 5);
+        debug = rgb.getConfig().getBoolean("core.debug", false);
+        requirePerms = rgb.getConfig().getBoolean("integration.force-perms-to-colorize", false);
+        storeUrl = rgb.getConfig().getString("integration.store-url", "none");
     }
 
     /**
      * Total # of colors pre-loaded for rainbow array
+     * 
      * @return how many colors we're using
      */
     public int getColors() {
@@ -29,6 +35,7 @@ public class Config {
 
     /**
      * Get the armor update speed
+     * 
      * @return armor refresh rate in ticks
      */
     public int getRefreshRate() {
@@ -37,10 +44,27 @@ public class Config {
 
     /**
      * Should we send debug messages?
+     * 
      * @return true if debugging is enabled
      */
     public boolean shouldDebug() {
         return debug;
     }
 
+    public boolean isIntegrating() {
+        return !storeUrl.equalsIgnoreCase("none");
+    }
+
+    public String getStoreLink() {
+        return storeUrl;
+    }
+
+    public boolean shouldForcePermsToColorize() {
+        return requirePerms;
+    }
+
+    @Override
+    public String toString() {
+        return "Config [storeUrl=" + storeUrl + ", debug=" + debug + ", forcePerms=" + requirePerms + ", refreshRate=" + refreshRate + ", colors=" + colors + "]";
+    }  
 }
